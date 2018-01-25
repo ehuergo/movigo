fe7c7cef142a0c34086bc31a00396229a8d360c3
Juanma Sanchez <jms@jBook.local>
2018-01-25 23:35:08 +0100

    remove -playlistfmt, rename -saveas to -savem3u

--- commandline.go
+++ commandline.go
@@ -10,10 +10,9 @@ type Opts struct{
                                     // udp://
                                     // path/to/file
 
-    saveto          URL             // stdout
+    savem3u         URL             // stdout
                                     // path/to/file
 
-    playlistfmt     string          // m3u
     streamaccess    URL             // udpxy | udp | rtp
 
     area            int             // see movistartv.go for area details
@@ -40,14 +39,13 @@ func parseCommandLine() *Opts{
     opts := &Opts{}
 
     flag.Var(&opts.readfrom, "readfrom", "Access method. udp:// reads straight from the network. udpxy://IP:PORT reads via udpxy proxy. Otherwise it's considered a file")
-    flag.Var(&opts.saveto, "saveto", "stdout Dumps the file to stdout. Otherwise it's considered a path in the filesystem. Defaults to stdout")
-    flag.StringVar(&opts.playlistfmt, "playlistfmt", "", "Only m3u is supported")
+    flag.Var(&opts.savem3u, "savem3u", "stdout Dumps the file to stdout. Otherwise it's considered a path in the filesystem. Defaults to stdout")
     flag.Var(&opts.streamaccess, "streamaccess", "udpxy://IP:PORT, udp:// or rtp://")
     flag.IntVar(&opts.area, "area", int(MADRID), "Area code")
     flag.BoolVar(&opts.listpackages, "l", false, "")
     flag.BoolVar(&opts.verbose, "v", false, "")
 
-    opts.saveto.Raw = "stdout"
+    opts.savem3u.Raw = "stdout"
 
     flag.Parse()
 
