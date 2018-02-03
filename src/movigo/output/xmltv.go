ae4c053c8102db290cc1c451a4fcefdaa448f956
Juanma Sanchez <jms@juanmasanchez.net>
2018-02-03 12:02:30 +0100

    Merge xmltv code

--- src/movigo/output/xmltv.go
+++ src/movigo/output/xmltv.go
@@ -2,16 +2,44 @@ package output
 
 import (
     "movigo/movi"
-    "movigo/epg"
     "fmt"
     "encoding/xml"
 )
 
+type XMLTVFile struct{
+    XMLName     xml.Name
+    Channel     []*XMLTVChannel     `xml:"channel"`
+    Programme   []*XMLTVProgramme   `xml:"programme"`
+}
+
+func NewXMLTVFile() *XMLTVFile{
+    xmltvf := &XMLTVFile{}
+    xmltvf.Channel = make([]*XMLTVChannel, 0)
+    xmltvf.Programme = make([]*XMLTVProgramme, 0)
+
+    return xmltvf
+}
+
+type XMLTVChannel struct{
+    Id      string  `xml:"id,attr"`
+    Name    string  `xml:"display-name"`
+}
+
+type XMLTVProgramme struct{
+    Start       string  `xml:"start,attr"`
+    Stop        string  `xml:"stop,attr"`
+    Channel     string  `xml:"channel,attr"`
+    Title       string  `xml:"title"`
+    SubTitle    string  `xml:"sub-title"`
+    Desc        string  `xml:"desc"`
+    Date        string  `xml:"date"`
+}
+
 func DumpXMLTVEPG(channels []*movi.LogicalChannel) []byte{
-    xmltvf := epg.NewXMLTVFile()
+    xmltvf := NewXMLTVFile()
     //xmltvf.XMLName.Local = "tv"
     for _, channel := range channels{
-        xmltvc := &epg.XMLTVChannel{}
+        xmltvc := &XMLTVChannel{}
         xmltvc.Id = fmt.Sprintf("%d", channel.Id)
         xmltvc.Name = channel.Name
         xmltvf.Channel = append(xmltvf.Channel, xmltvc)
@@ -19,7 +47,7 @@ func DumpXMLTVEPG(channels []*movi.LogicalChannel) []byte{
         if channel.EPG == nil || len(channel.EPG.Programs) == 0{ continue }
 
         for _, program := range channel.EPG.Programs{
-             xmltvp := &epg.XMLTVProgramme{}
+             xmltvp := &XMLTVProgramme{}
              xmltvp.Start = program.Start.Format("20060102150405 -0700")
              xmltvp.Stop = program.Start.Add(program.Duration).Format("20060102150405 -0700")
              xmltvp.Channel = fmt.Sprintf("%d", channel.Id) //fmt.Sprintf("%d", channel.EPG.File.ServiceId)
