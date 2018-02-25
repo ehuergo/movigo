d63674515d0617be81a3acfaf9e20b6a38b3ab21
Juanma Sanchez <jms@juanmasanchez.net>
2018-02-25 02:01:13 +0100

    Use xmltv package from xmltvtool

--- output/xmltv.go
+++ output/xmltv.go
@@ -4,42 +4,14 @@ import (
     "../movi"
     "fmt"
     "encoding/xml"
+    "github.com/juanmasg/xmltvtool/xmltv"
 )
 
-type XMLTVFile struct{
-    XMLName     xml.Name
-    Channel     []*XMLTVChannel     `xml:"channel"`
-    Programme   []*XMLTVProgramme   `xml:"programme"`
-}
-
-func NewXMLTVFile() *XMLTVFile{
-    xmltvf := &XMLTVFile{}
-    xmltvf.Channel = make([]*XMLTVChannel, 0)
-    xmltvf.Programme = make([]*XMLTVProgramme, 0)
-
-    return xmltvf
-}
-
-type XMLTVChannel struct{
-    Id      string  `xml:"id,attr"`
-    Name    string  `xml:"display-name"`
-}
-
-type XMLTVProgramme struct{
-    Start       string  `xml:"start,attr"`
-    Stop        string  `xml:"stop,attr"`
-    Channel     string  `xml:"channel,attr"`
-    Title       string  `xml:"title"`
-    SubTitle    string  `xml:"sub-title"`
-    Desc        string  `xml:"desc"`
-    Date        string  `xml:"date"`
-}
-
 func DumpXMLTVEPG(channels []*movi.LogicalChannel) []byte{
-    xmltvf := NewXMLTVFile()
+    xmltvf := xmltv.NewXMLTVFile()
     //xmltvf.XMLName.Local = "tv"
     for _, channel := range channels{
-        xmltvc := &XMLTVChannel{}
+        xmltvc := &xmltv.Channel{}
         xmltvc.Id = fmt.Sprintf("%d", channel.Id)
         xmltvc.Name = channel.Name
         xmltvf.Channel = append(xmltvf.Channel, xmltvc)
@@ -47,7 +19,7 @@ func DumpXMLTVEPG(channels []*movi.LogicalChannel) []byte{
         if channel.EPG == nil || len(channel.EPG.Programs) == 0{ continue }
 
         for _, program := range channel.EPG.Programs{
-             xmltvp := &XMLTVProgramme{}
+             xmltvp := &xmltv.Programme{}
              xmltvp.Start = program.Start.Format("20060102150405 -0700")
              xmltvp.Stop = program.Start.Add(program.Duration).Format("20060102150405 -0700")
              xmltvp.Channel = fmt.Sprintf("%d", channel.Id) //fmt.Sprintf("%d", channel.EPG.File.ServiceId)
